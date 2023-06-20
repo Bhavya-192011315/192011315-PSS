0ce0853ee633a4ea926c8e8ebc48ff4e37a60c27
Bhavya-192011315 <123440378+Bhavya-192011315@users.noreply.github.com>
2023-06-20 09:10:46 +0530

    Add files via upload

---
+++ PSS pro 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int i;
+	printf("natural numbers upto 50 with a different of 2: ");
+	for(i=1;i<=50;i+=2)
+	{
+		printf("%d\n ",i);
+
+	}
+}
---
+++ pss pro 7 fact.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int factorial(int n);
+int main()
+{
+    int n=8;
+    printf("factorial of %d: %d",n,factorial(n));
+    return 0;
+    }
+int factorial(int n){
+    if(n==0||n==1){
+       return 1;
+    }else{
+       return n* factorial(n-1);
+       }
+}
+
